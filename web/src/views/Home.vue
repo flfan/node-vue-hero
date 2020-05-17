9d0a3a4ffb068d6b2dd7b23c4f149663af1e5037
flfan <1490459938@qq.com>
2020-05-17 21:01:43 +0800

    web home swiper

---
+++ web/src/views/Home.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="home">
+    <swiper ref="mySwiper" :options="swiperOptions">
+    <swiper-slide>
+      <img class="w-100" src="../assets/images/3f68e30e33a23af8d74b3f334dd210a6.jpeg">
+    </swiper-slide>
+    <swiper-slide>
+      <img class="w-100" src="../assets/images/3f2262d6d3c564a621b385eac3de6152.jpeg">
+    </swiper-slide>
+    <swiper-slide>
+      <img class="w-100" src="../assets/images/ee49f764f4122f73dbe122d3f6e2dcc1.jpeg">
+    </swiper-slide>
+    <div class="swiper-pagination swiper-pagination-home text-right px-3 pb-2" slot="pagination"></div>
+  </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      swiperOptions: {
+        pagination: {
+          el: '.swiper-pagination-home',
+          clickable: true,
+        },
+        autoplay: {
+          delay: 2000,
+          stopOnLastSlide: false,
+          disableOnInteraction: true,
+        },
+        loop: true
+      }
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '../assets/style/_variable.styl'
+.home >>>
+  .swiper-pagination-home
+    .swiper-pagination-bullet
+      border-radius 0.1538rem
+      opacity 1
+      background #ffffff
+      margin-left 1rem
+      &.swiper-pagination-bullet-active
+        background $colors.info
+</style>
---
+++ web/src/views/Main.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="main">
+    <div class="topbar bg-dark-1 px-3 py-2 d-flex ai-center">
+      <div class="logo">
+        <img src="../assets/logo.png" height="30">
+      </div>
+      <div class="text pl-3 d-flex ai-stretch flex-column jc-between flex-1">
+        <div class="title text-white mb-1">王者荣耀</div>
+        <div class="desc text-grey-1 fs-xxs">团队成就更多</div>
+      </div>
+      <div class="button">
+        <button type="button" class="btn bg-primary">立即下载</button>
+      </div>
+    </div>
+    <div class="nav d-flex jc-around bg-primary text-white pt-3 pb-2">
+      <div class="nav-item active">
+        <router-link tag="div" class="nav-link" to='/'>首页</router-link>
+      </div>
+      <div class="nav-item">
+        <router-link tag="div" class="nav-link" to='/'>攻略中心</router-link>
+      </div>
+      <div class="nav-item">
+        <router-link tag="div" class="nav-link" to='/'>赛事中心</router-link>
+      </div>
+    </div>
+    <router-view></router-view>
+  </div>
+</template>
