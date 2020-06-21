4968c30f926ce41afaafe80e7e631f142fefba65
flfan <1490459938@qq.com>
2020-06-21 22:23:04 +0800

    fix

--- admin/src/views/CategoryList.vue
+++ admin/src/views/CategoryList.vue
@@ -8,7 +8,7 @@
       <el-table-column fixed="right" label="操作" width="180">
         <template slot-scope="scope">
           <el-button type="primary" size="small" @click="$router.push(`/categories/edit/${scope.row._id}`)">编辑</el-button>
-        <el-button type="primary" size="small" @click="remove(scope.row)">删除</el-button>
+          <el-button type="primary" size="small" @click="remove(scope.row)">删除</el-button>
         </template>
       </el-table-column>
     </el-table>
@@ -29,25 +29,25 @@ export default {
       this.items = res.data;
     },
     remove(row) {
-        this.$confirm(`此操作将永久删除分类${row.name}, 是否继续?`, '提示', {
-          confirmButtonText: '确定',
-          cancelButtonText: '取消',
-          type: 'warning'
-        }).then(async () => {
+      this.$confirm(`此操作将永久删除分类${row.name}, 是否继续?`, '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(async () => {
 
-          await this.$http.delete(`/rest/categories/${row._id}`)
-          this.fetch()
-          this.$message({
-            type: 'success',
-            message: '删除成功!'
-          });
-        }).catch(() => {
-          this.$message({
-            type: 'info',
-            message: '已取消删除'
-          });          
+        await this.$http.delete(`/rest/categories/${row._id}`)
+        this.fetch()
+        this.$message({
+          type: 'success',
+          message: '删除成功!'
         });
-      }
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '已取消删除'
+        });          
+      });
+    }
   },
   created() {
     this.fetch();
